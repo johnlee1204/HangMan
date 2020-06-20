384eb075f46b0e32debadc8b35d880b0353152ff
John Lee <johnmlee1204@gmail.com>
2020-06-20 13:40:23 -0600

    Added txt files of words to choose from.
    Added some empty function the program will use

---
+++ HangManDriver.py
@@ -0,0 +1,25 @@
+def drawHangMan():
+    # TODO Draw the hangman using the current number of wrong guesses
+    print("Hello")
+
+
+def listPastWrongGuesses():
+    # TODO List all the wrong letters the user has guessed
+    print("Hello")
+
+
+def promptUserForLetterGuess():
+    # TODO Get the users guess as a letter
+    print("Hello")
+
+def promptUserForAnswerGuess():
+    # TODO Get the users guess the word
+    print("Hello")
+
+def selectWordFromWordList():
+    # TODO Select a word from the words txt file for the user to guess
+    print("Hello")
+
+def selectWordFromWordList():
+    # TODO Select a word from the words txt file for the user to guess
+    print("Hello")

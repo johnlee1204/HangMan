1bed50c1c52d490a32ef18b0052d91fb7f5b48bf
TimHuxtable <tph4@illinois.edu>
2020-06-20 20:03:09 -0500

    John

--- HangManDriver.py
+++ HangManDriver.py
@@ -1,7 +1,33 @@
+import random
+wordlist = []
+guessedCharacters = []
 def drawHangMan():
     # TODO Draw the hangman using the current number of wrong guesses
     print("Hello")
+def answerSection(word):
+    answerSectionString = ""
+    for letter in word:
+        try:
+            index = guessedCharacters.index(letter)
+            answerSectionString += letter
+        except ValueError:
+            answerSectionString += "_"
+    print(answerSectionString)
+    return answerSectionString.find("_")
 
+
+def guessLetter(character):
+    if word.find(character) == -1:
+        print("guess again")
+    else:
+        print("correct")
+        guessedCharacters.append(character)
+        return "correct"
+    try:
+        index = guessedCharacters.index(character)
+        return "duplicate guess"
+    except ValueError:
+        guessedCharacters.append(character)
 
 def listPastWrongGuesses():
     # TODO List all the wrong letters the user has guessed
@@ -16,10 +42,42 @@ def promptUserForAnswerGuess():
     # TODO Get the users guess the word
     print("Hello")
 
-def selectWordFromWordList():
-    # TODO Select a word from the words txt file for the user to guess
-    print("Hello")
+def selectWordFromWordlist():
+    return wordlist[int(random.random()*1000)]
 
-def selectWordFromWordList():
+def putWordsInWordlist():
     # TODO Select a word from the words txt file for the user to guess
-    print("Hello")
+    with open('wordList.txt','r') as file:
+        for word in file:
+            wordlist.append(word)
+
+putWordsInWordlist()
+word = selectWordFromWordlist()
+word = word[0:len(word) - 1]
+# print(word)
+# print(len(word))
+
+
+answerSection(word)
+i = 0
+x = len(word)
+numberOfTries = int((1/x) * 30 + 3)
+if numberOfTries > 15:
+    numberOfTries = 15
+while i < numberOfTries:
+    print("Guess a Character, you have " + str(numberOfTries - i) + " attempt(s) left. You have guessed: " + " ".join(guessedCharacters))
+    characterGuess = input()
+    guessLetterResult = guessLetter(characterGuess)
+
+    if guessLetterResult == "duplicate guess":
+        print("hey retard guess again")
+        continue
+    elif guessLetterResult == "correct":
+        i = i - 1
+    if answerSection(word) == -1:
+        print("you win!")
+        break
+    if i == numberOfTries - 1:
+        print("you lost!")
+    i += 1
+print(word)

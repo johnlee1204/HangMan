672cb8b8e6d380577f00c9ef0f9fab68dab64a52
John Lee <johnmlee1204@gmail.com>
2020-06-20 21:16:18 -0500

    Added template client and server code

--- client.py
+++ client.py
@@ -1,15 +1,56 @@
-import asyncio
-import websockets
+import sys
 
-async def hello():
-    uri = "ws://localhost:8765"
-    async with websockets.connect(uri) as websocket:
-        name = input("What's your name? ")
+# Import socket library
+from socket import *
 
-        await websocket.send(name)
-        print(f"> {name}")
+if sys.argv.__len__() != 3:
+    serverName = 'localhost'
+    serverPort = 5895
+# Get from command line
+else:
+    serverName = sys.argv[1]
+    serverPort = int(sys.argv[2])
 
-        greeting = await websocket.recv()
-        print(f"< {greeting}")
+# Choose SOCK_STREAM, which is TCP
+clientSocket = socket(AF_INET, SOCK_STREAM)
 
-asyncio.get_event_loop().run_until_complete(hello())
+# Connect to server using hostname/IP and port
+clientSocket.connect((serverName, serverPort))
+
+#Recieves lines of words
+linesInBytes = clientSocket.recv(1024)
+lines = linesInBytes.decode('utf-8')
+linesString = ""
+for x in lines:
+    linesString += x + " "
+# linesString = linesString[0:len(linesString) - 1]
+print(linesString)
+
+while 1:
+
+    # Get letter from user
+    print('\n')
+    letter = input('Guess a letter: ')
+
+    # Sends letter
+    letterBytes = letter.encode('utf-8')
+    clientSocket.send(letterBytes)
+
+    #Recieves newWord
+    newWordInBytes = clientSocket.recv(len(linesInBytes))
+    newWord = newWordInBytes.decode('utf-8')
+
+    if newWord.find("_"):
+
+        linesString = ""
+        for x in newWord:
+            linesString += x + " "
+        # linesString = linesString[0:len(linesString) - 1]
+    else:
+        linesString = newWord
+
+    print(newWord)
+    if(linesString == 'You have won the game'):
+        print(newWord)
+        clientSocket.close()
+        break
--- server.py
+++ server.py
@@ -1,16 +1,133 @@
-import asyncio
-import websockets
+import sys
+import random
 
-async def hello(websocket, path):
-    name = await websocket.recv()
-    print(f"< {name}")
+# Import socket library
+from socket import *
 
-    greeting = f"Hello {name}!"
+wordlist = []
+guessedCharacters = []
 
-    await websocket.send(greeting)
-    print(f"> {greeting}")
 
-start_server = websockets.serve(hello, "localhost", 8765)
 
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+def answerSection(word):
+    answerSectionString = ""
+    for letter in word:
+        try:
+            index = guessedCharacters.index(letter)
+            answerSectionString += letter
+        except ValueError:
+            answerSectionString += "_"
+    print(answerSectionString)
+    return answerSectionString.find("_")
+
+
+def guessLetter(character):
+    try:
+        guessedCharacters.index(character)
+        return "duplicate guess"
+    except ValueError:
+        guessedCharacters.append(character)
+
+    if word.find(character) == -1:
+        print("guess again")
+    else:
+        print("correct")
+        guessedCharacters.append(character)
+        return "correct"
+
+def selectWordFromWordlist():
+    return wordlist[int(random.random()*1000)]
+
+def putWordsInWordlist():
+    with open('wordList.txt','r') as file:
+        for word in file:
+            wordlist.append(word)
+
+
+if sys.argv.__len__() != 2:
+    serverPort = 5895
+# Get port number from command line
+else:
+    serverPort = int(sys.argv[1])
+
+# Choose SOCK_STREAM, which is TCP
+# This is a welcome socket
+serverSocket = socket(AF_INET, SOCK_STREAM)
+
+serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+
+# Start listening on specified port
+serverSocket.bind(('', serverPort))
+
+# Listener begins listening
+serverSocket.listen(1)
+
+print("The server is ready to receive")
+
+#Set secret word
+putWordsInWordlist()
+word = selectWordFromWordlist()
+word = word[0:len(word) - 1]
+print(word)
+linesForString = ''
+#Prints out number of letters
+for x in word:
+    linesForString += '_'
+
+newWord = word
+
+# Wait for connection and create a new socket
+# It blocks here waiting for connection
+connectionSocket, addr = serverSocket.accept()
+win = ' '
+#Sends lines of words
+linesInBytes = linesForString.encode('utf-8')
+connectionSocket.send(linesInBytes)
+
+lose = 0
+while 1:
+
+
+    l = list(word)
+    list2 = list(linesForString)
+
+    win = False
+
+    while 1:
+
+        while win == False:
+            losee = 0
+            # Receives Letter
+            letter = connectionSocket.recv(1024)
+            letterString = letter.decode('utf-8')
+            print(letterString)
+
+            guessLetterResult = guessLetter(letterString)
+            messageForUser = ""
+            if guessLetterResult == "duplicate guess":
+                messageForUser += "hey retard guess again"
+
+            #Sends newWord
+
+            answerSectionString = ""
+            for letter in word:
+                try:
+                    index = guessedCharacters.index(letter)
+                    answerSectionString += letter
+                except ValueError:
+                    answerSectionString += "_"
+
+            messageForUser += answerSectionString
+
+            if answerSectionString.find("_") == -1:
+                win = True
+                winGame = 'You have won the game'
+                winGameInBytes = winGame.encode('utf-8')
+                connectionSocket.send(winGameInBytes)
+                connectionSocket.close()
+            else:
+                messageForUserBytes = messageForUser.encode('utf-8')
+                connectionSocket.send(messageForUserBytes)
+
+        break
+    break
